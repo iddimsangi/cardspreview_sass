f01d7b36d11e55f6a91b0c76eb376023835f64c1
iddimsangi <adammsangi@gmail.com>
2021-12-08 09:50:12 +0300

    add images and design cards

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 
 import './App.scss';
-
+import icon_luxuary from './images/icon-luxury.svg'
+import icon_sedans from './images/icon-sedans.svg'
+import icon_suvs from './images/icon-suvs.svg'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>hello world</h1>
-      </header>
+      <div className="App-container">
+        <div className="App-container--card">
+          <img src={icon_sedans} alt="icon_sedans" />
+          <h1>SEDANS</h1>
+          <p>Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.</p>
+          <button>Learn More</button>
+        </div>
+        <div className="App-container--card">
+        <img src={icon_suvs} alt="icon_suvs" />
+        <h1>SUVS</h1>
+          <p>Take an SUV for its spacious interior, power and versatility. Perfect for your next family vacation and off-road adventures.</p>
+          <button>Learn More</button>
+        </div>
+        <div className="App-container--card">
+        <img src={icon_luxuary} alt="icon_luxuary" />
+
+        <h1>LUXURY</h1>
+          <p>Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxuiry rental and arrive in style.</p>
+        <button>Learn More</button>
+        </div>
+      </div>
     </div>
   );
 }
