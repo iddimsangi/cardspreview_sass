8e860ac441a633b383973123d884a6338baa6aaf
iddimsangi <adammsangi@gmail.com>
2021-12-08 11:48:04 +0300

    add styles to cards and background colors

--- src/App.js
+++ src/App.js
@@ -7,19 +7,19 @@ function App() {
   return (
     <div className="App">
       <div className="App-container">
-        <div className="App-container--card">
+        <div className="App-container--card-1">
           <img src={icon_sedans} alt="icon_sedans" />
           <h1>SEDANS</h1>
           <p>Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.</p>
           <button>Learn More</button>
         </div>
-        <div className="App-container--card">
+        <div className="App-container--card-2">
         <img src={icon_suvs} alt="icon_suvs" />
         <h1>SUVS</h1>
           <p>Take an SUV for its spacious interior, power and versatility. Perfect for your next family vacation and off-road adventures.</p>
           <button>Learn More</button>
         </div>
-        <div className="App-container--card">
+        <div className="App-container--card-3">
         <img src={icon_luxuary} alt="icon_luxuary" />
 
         <h1>LUXURY</h1>
